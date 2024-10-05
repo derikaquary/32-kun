ef47dc7eb4051e89636cfd8fcf2d09af564bafad
derikaquary <derikaquary@gmail.com>
2024-10-05 10:16:20 +0000

    deleted the footer on the page

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ const outfit = Outfit({
 export default function Home() {
   return (
     <main
-      className={`${openSans.className} relative flex h-[100vh] w-full flex-col items-center justify-center gap-3`}
+      className={`${openSans.className} relative flex h-[100vh] w-full flex-col items-center justify-center`}
     >
       {" "}
       <Image
@@ -26,9 +26,8 @@ export default function Home() {
         fill
         className="z-[-50] object-cover"
       />
-      <div className="flex-1 w-full"></div>
       <div className="flex flex-col items-center">
-        <div className="flex h-[200px] w-[250px] items-center justify-center rounded-xl bg-[#dfdfdf] p-2 mb-3">
+        <div className="mb-3 flex h-[200px] w-[250px] items-center justify-center rounded-xl bg-[#dfdfdf] p-2">
           <div className="relative w-full h-full">
             <Image
               src={kunImage}
@@ -40,23 +39,6 @@ export default function Home() {
         </div>
         <p className="text-6xl font-[900] text-secondary">COMPANY</p>
         <p className="text-6xl font-[900] text-white">PROFILE</p>
-      </div>
-      <div className="flex flex-col flex-1 justify-end items-center w-full">
-        <div className="flex-1 w-full"></div>
-        <div className="flex-1 w-full"></div>
-        {/* Footer */}
-        <div
-          className={`flex w-full flex-1 flex-row items-center justify-between gap-7 ${outfit.className} max-w-7xl px-3 py-3`}
-        >
-          <p className="flex-0.7 text-center font-[200] text-white sm:text-3xl">
-            01/29
-          </p>
-          <div className="h-[2px] w-full flex-1 bg-white"></div>
-          <p className="flex-0.7 text-center font-[300] text-white sm:text-3xl">
-            <span className="sm:hidden">KUN</span>
-            <span className="hidden sm:block">Kreator Utama Nusantara</span>
-          </p>
-        </div>
       </div>
     </main>
   );
