96eaa299268531501ea48694147175898b4194a0
derikaquary <derikaquary@gmail.com>
2024-10-08 02:31:11 +0000

    Finished the 1st page of service

--- app/service/page.js
+++ app/service/page.js
@@ -10,7 +10,17 @@ function Service() {
         fill
         className="z-[-50] object-cover"
       />
-      <div className="flex flex-col gap-8 justify-center items-center p-2 rounded-lg bg-black/30">
+      {/* Big Screen */}
+      <div className="hidden flex-col justify-center items-center p-2 w-full rounded-lg sm:flex">
+        <div className="w-full h-[100px] text-[55px] font-[800] text-secondary flex items-center pl-[60px]">Our Services</div>
+        <div className="gap-3 w-full h-[200px]  text-4xl font-[500] text-white pl-[200px] flex flex-col justify-center "><p>VIDEO PRODUCTION</p><p>BRAND ACTIVATION</p><p>CONTENT CREATOR</p></div>
+        <div className="w-full h-[100px]  text-4xl font-[500] text-white  pl-[200px]"></div>
+        <div className="w-full h-[100px]  text-4xl font-[500] text-white  pl-[200px]"></div>
+        
+        
+      </div>
+      {/* Small Screen */}
+      <div className="flex flex-col gap-8 justify-center items-center p-2 rounded-lg sm:hidden bg-black/30">
         <p className="text-5xl font-[800] text-secondary">Our Services</p>
         <div className="flex flex-col gap-1 text-3xl font-[500] text-white">
           <p>VIDEO PRODUCTION</p>
