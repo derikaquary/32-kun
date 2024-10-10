4166f82821d80a3f1ef5da0d0dea958e2e09d33f
derikaquary <derikaquary@gmail.com>
2024-10-10 01:28:21 +0000

    finish page 3 branding small screen

--- app/_components/Page3Brand.js
+++ app/_components/Page3Brand.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import page from "@/public/page 1_14.png";
 import Link from "next/link";
-import rapat from "@/public/rapat.png";
+import employee from "@/public/employee.png";
 import gong from "@/public/gong.png";
 import { Outfit } from "next/font/google";
 
@@ -10,7 +10,7 @@ const outfit = Outfit({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-function Page2Brand() {
+function PageBrand3() {
   return (
     <div
       className={`relative flex w-full flex-col items-center justify-center ${outfit.className}`}
@@ -26,17 +26,17 @@ function Page2Brand() {
         <div className="flex h-[100px] w-full items-center text-[55px] font-[800] text-secondary">
           EVENT
         </div>
-        <p className="w-[700px] text-[35px] font-[800] text-tertiery leading-[1.2] mb-2">
+        <p className="mb-2 w-[700px] text-[35px] font-[800] leading-[1.2] text-tertiery">
           Opening Ceremony Rakernis Kementerian Ketenagakerjaan RI
         </p>
         <div className="flex flex-col gap-4">
-          <div className="gap-3 flex h-[330px] w-full max-w-7xl ">
+          <div className="flex h-[330px] w-full max-w-7xl gap-3">
             {/* Left box */}
             <div className="flex-1 h-200px">
-              <div className="relative w-full h-[330px]">
+              <div className="relative h-[330px] w-full">
                 <Image
-                  src={rapat}
-                  alt="gambar YouTube rapat"
+                  src={employee}
+                  alt="gambar YouTube employee"
                   fill
                   className="object-fit"
                 />
@@ -68,51 +68,53 @@ function Page2Brand() {
         </div>
       </div>
       {/* Small Screen */}
-      <div className="flex flex-col gap-3 justify-center px-2 py-8 w-full bg-black/10 sm:hidden">
-        <p className="text-center text-5xl font-[800] text-secondary">EVENT</p>
-        <p className="text-center text-3xl font-[700] text-tertiery leading-[1.0]">
-          Opening Ceremony Rakernis Kementerian Ketenagakerjaan RI
+      <div className="flex flex-col gap-3 justify-center px-2 pt-8 w-full border-t-2 border-white/40 bg-black/10 sm:hidden">
+        <p className=" text-center text-3xl font-[700] leading-[1.0] text-tertiery">
+          Employee Gathering PGN
         </p>
-        <div className="h-[390px] w-full">
-          <div className="relative h-[230px] w-full">
-            <Image
-              src={rapat}
-              alt="gambar YouTube rapat"
-              fill
-              className="object-fit"
-            />
-          </div>
-
+        <div className="w-full">
+          <Link
+            href="https://www.youtube.com/watch?v=sljShumOgOE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative h-[230px] w-full">
+              <Image
+                src={employee}
+                alt="gambar YouTube employee"
+                fill
+                className="object-fit"
+              />
+            </div>
+          </Link>
+          <p
+            className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            Pre&#45;Production
+          </p>
           <ul
             className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
           >
             <li>
-              Pre&#45;Production &#40; Concepting&#44; Script Writing &#38;
-              Voice Over &#41;
+              Concepting &#40; Rundown&#44; Script Writing and Games &#41;
             </li>
-            <li>Production &#40; Shooting&#44; Directing and Briefing&#41;</li>
-            <li>Post&#45;Production &#40; Editing Motion &#41;</li>
+            <li>Talent Search</li>
+            <li>Stage Design</li>
+            <li>Property</li>
+            <li>Location</li>
           </ul>
-        </div>
-        <div className="flex-1 mt-5 h-400px">
-          <div className="relative h-[200px] w-full">
-            <Image
-              src={gong}
-              alt="gambar YouTube rapat"
-              fill
-              className="object-fit"
-            />
-          </div>
-
+          <p
+            className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            Production
+          </p>
           <ul
             className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
           >
-            <li>
-              Pre&#45;Production &#40; Concepting&#44; Script Writing &#38;
-              Voice Over &#41;
-            </li>
-            <li>Production &#40; Shooting&#44; Directing and Briefing&#41;</li>
-            <li>Post&#45;Production &#40; Editing Motion &#41;</li>
+            <li>Live Streaming system on YouTube</li>
+            <li>Talent Brief</li>
+            <li>Technical System</li>
+            <li>Games Coordinator</li>
           </ul>
         </div>
       </div>
@@ -120,4 +122,4 @@ function Page2Brand() {
   );
 }
 
-export default Page2Brand;
+export default PageBrand3;
--- app/service/brand_activation/page.js
+++ app/service/brand_activation/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import page from "@/public/page 1_13.png";
 import Page1Brand from "@/app/_components/Page1Brand";
 import Page2Brand from "@/app/_components/Page2Brand";
+import Page3Brand from "@/app/_components/Page3Brand";
 
 function BrandActivation() {
   return (
     <>
       <Page1Brand />
       <Page2Brand />
+      <Page3Brand/>
     </>
   );
 }
