c4439e64e88732750c658ac9938fb097277b9373
derikaquary <derikaquary@gmail.com>
2024-10-10 07:50:49 +0000

    finished page 4 brand

--- app/_components/Page5Brand.js
+++ app/_components/Page5Brand.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import page from "@/public/page 1_14.png";
 import Link from "next/link";
-import employee from "@/public/employee.png";
+import jejer from "@/public/jejer.png";
 import { Outfit } from "next/font/google";
 
 const outfit = Outfit({
@@ -9,9 +9,9 @@ const outfit = Outfit({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-function PageBrand3() {
-  return (
-    <div
+function Page5Brand() {
+    return (
+        <div
       className={`relative flex w-full flex-col items-center justify-center ${outfit.className}`}
     >
       <Image
@@ -21,13 +21,13 @@ function PageBrand3() {
         className="z-[-50] object-cover"
       />
       {/* Big Screen */}
-      <div className=" hidden h-[100vh] w-full max-w-7xl flex-col items-center justify-center sm:flex ">
-        <div className="flex h-[70px] w-[1000px] items-center text-[35px] font-[700] text-secondary">
+      <div className="hidden h-[100vh] w-full max-w-7xl flex-col items-center justify-center sm:flex">
+        <div className="flex h-[70px] w-[850px] items-center text-[35px] font-[700] text-secondary">
           EVENT
         </div>
-        <div className="w-[1000px] ">
+        <div className="w-[850px]">
           <p className="mb-2 text-2xl font-[800] leading-[1.2] text-tertiery">
-            Employee Gathering PGN
+            Grand Launching panganan.com Bulog
           </p>
         </div>
         <div className="flex gap-4 justify-center items-center">
@@ -38,15 +38,11 @@ function PageBrand3() {
               Pre&#45;Production
             </p>
             <ul
-              className={`ml-5 mt-2 flex list-disc flex-col px-2 text-lg font-[200] text-white ${outfit.className}`}
+              className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
             >
-              <li>
-                Concepting &#40; Rundown&#44; Script Writing and Games &#41;
-              </li>
-              <li>Talent Search</li>
+              <li>Concepting</li>
+              <li>Rundown and Script Writing</li>
               <li>Stage Design</li>
-              <li>Property</li>
-              <li>Location</li>
             </ul>
             <p
               className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
@@ -54,32 +50,33 @@ function PageBrand3() {
               Production
             </p>
             <ul
-              className={`ml-5 mt-2 flex list-disc flex-col px-2 text-lg font-[200] text-white ${outfit.className}`}
+              className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
             >
-              <li>Live Streaming system on YouTube</li>
+              <li>Manpower</li>
+              <li>Show Management</li>
               <li>Talent Brief</li>
               <li>Technical System</li>
-              <li>Games Coordinator</li>
+              <li>Documentation</li>
             </ul>
           </div>
           {/* Right Box */}
-          
-            <div className="relative h-[300px] w-[500px] ">
-              <Image
-                src={employee}
-                alt="gambar YouTube employee"
-                fill
-                className="object-fit"
-              />
-            </div>
-          
+
+          <div className="relative h-full w-[455px]">
+            <Image
+              src={jejer}
+              alt="gambar YouTube jejer"
+              fill
+              className="object-fit"
+            />
+          </div>
+
           <div className="flex justify-center"></div>
         </div>
       </div>
       {/* Small Screen */}
       <div className="flex flex-col gap-3 justify-center px-2 py-8 w-full border-t-2 border-white/40 bg-black/10 sm:hidden">
         <p className="text-center text-3xl font-[700] leading-[1.0] text-tertiery">
-          Employee Gathering PGN
+          Grand Launching Panganan.com Bulog
         </p>
         <div className="w-full">
           <Link
@@ -89,8 +86,8 @@ function PageBrand3() {
           >
             <div className="relative h-[230px] w-full">
               <Image
-                src={employee}
-                alt="gambar YouTube employee"
+                src={jejer}
+                alt="gambar YouTube jejer"
                 fill
                 className="object-fit"
               />
@@ -104,13 +101,9 @@ function PageBrand3() {
           <ul
             className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
           >
-            <li>
-              Concepting &#40; Rundown&#44; Script Writing and Games &#41;
-            </li>
-            <li>Talent Search</li>
+            <li>Concepting</li>
+            <li>Rundown and Script Writing</li>
             <li>Stage Design</li>
-            <li>Property</li>
-            <li>Location</li>
           </ul>
           <p
             className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
@@ -120,15 +113,16 @@ function PageBrand3() {
           <ul
             className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
           >
-            <li>Live Streaming system on YouTube</li>
+            <li>Manpower</li>
+            <li>Show Management</li>
             <li>Talent Brief</li>
             <li>Technical System</li>
-            <li>Games Coordinator</li>
+            <li>Documentation</li>
           </ul>
         </div>
       </div>
     </div>
-  );
+    )
 }
 
-export default PageBrand3;
+export default Page5Brand
--- app/service/brand_activation/page.js
+++ app/service/brand_activation/page.js
@@ -3,6 +3,7 @@ import page from "@/public/page 1_13.png";
 import Page1Brand from "@/app/_components/Page1Brand";
 import Page2Brand from "@/app/_components/Page2Brand";
 import Page3Brand from "@/app/_components/Page3Brand";
+import Page4Brand from "@/app/_components/Page4Brand";
 
 function BrandActivation() {
   return (
@@ -10,6 +11,7 @@ function BrandActivation() {
       <Page1Brand />
       <Page2Brand />
       <Page3Brand/>
+      <Page4Brand/>
     </>
   );
 }
