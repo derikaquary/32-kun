0a969e6ec7609dda31e9051b403d9213ba8aec40
derikaquary <derikaquary@gmail.com>
2024-10-11 10:08:30 +0000

    change to no flex-1 in small screen service

--- app/service/layout.js
+++ app/service/layout.js
@@ -3,13 +3,15 @@ import Page1Service from "../_components/Page1Service"
 function ServiceLayout({children}) {
     return (
         <>
+        {/* BIG Screen */}
         <div className="hidden flex-row sm:flex">
             <Page1Service/>
             <div className="ml-[280px] flex-1">{children}</div>
         </div>
+        {/* Small Screen */}
         <div className="flex flex-col sm:hidden">
             <Page1Service/>
-            <div className="sm:ml-[280px] flex-1">{children}</div>
+            <div className="sm:ml-[280px] sm:flex-1">{children}</div>
         </div>
         
         </>
