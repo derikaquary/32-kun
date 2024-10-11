acacd4549f235cabf297bb080fd5def784ed39a1
derikaquary <derikaquary@gmail.com>
2024-10-11 12:42:30 +0000

    testing again

--- app/service/layout.js
+++ app/service/layout.js
@@ -1,16 +1,16 @@
-import Page1Service from "../_components/Page1Service"
+import NavService from "../_components/NavService"
 
 function ServiceLayout({children}) {
     return (
         <>
         {/* BIG Screen */}
         <div className="hidden flex-row sm:flex">
-            <Page1Service/>
+            <NavService/>
             <div className="ml-[280px] flex-1">{children}</div>
         </div>
         {/* Small Screen */}
-        <div className="flex flex-col sm:hidden">
-            <Page1Service/>
+        <div className="flex flex-col bg-red-400 sm:hidden">
+            <NavService/>
             <div className="sm:ml-[280px] flex-1">{children}</div>
         </div>
         
