e42319614081de242947ef7bbc5778fde25fc6bc
derikaquary <derikaquary@gmail.com>
2024-10-12 03:12:48 +0000

    changed the quotes to code in "Code"

--- app/service/page.js
+++ app/service/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import Page2Service from "../_components/Page2Service";
 import service from "@/public/service.jpeg"
 
 function Service() {
   return (
     <div className="px-2 sm:hidden bg-black/30">
-      <p className="px-2 py-5 text-3xl font-[700] text-secondary text-center "> Please "Click" one of the option above</p>
+      <p className="px-2 py-5 text-3xl font-[700] text-secondary text-center "> Please &#34;Click&#34; one of the option above</p>
       <div className="relative w-full h-[400px] "><Image src={service} alt="service image" fill className="object-cover rounded-xl"/></div>
     </div>
   );
