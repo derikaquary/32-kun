b92ded7f0eb067474d4474e6770377e2efb4bf69
derikaquary <derikaquary@gmail.com>
2024-10-12 09:37:07 +0000

    finished page 10 brand

--- app/_components/Page10Brand.js
+++ app/_components/Page10Brand.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import page from "@/public/page 1_14.png";
-import hut56 from "@/public/hut56.png";
+import milenial from "@/public/milenial.png";
 import { Outfit } from "next/font/google";
 
 const outfit = Outfit({
@@ -26,80 +26,13 @@ function Page10Brand() {
         </div>
         <div className="w-[910px]">
           <p className="mb-2 text-2xl font-[800] leading-[1.2] text-tertiery">
-            FUN MORNING BULOG
+            MILLENIALS GATHERING BULOG
           </p>
         </div>
         <div className="flex gap-9 justify-center items-center">
           <div className="flex text-[15px]">
             <div className="flex flex-col">
-              <p
-                className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
-              >
-                Pre&#45;Production
-              </p>
-              <ul
-                className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
-              >
-                <li>Concepting &#40;Rundown &#38; Script Writing&#41;</li>
-                <li>Stage Design</li>
-                <li>Property</li>
-                <li>Location</li>
-                <li>Talent Search</li>
-              </ul>
-              <p
-                className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
-              >
-                Production
-              </p>
-              <ul
-                className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
-              >
-                <li>Manpower Team</li>
-                <li>Show Directing</li>
-                <li>Talent Brief</li>
-                <li>Technical System</li>
-                <li>Documentation photo and video</li>
-              </ul>
-              <p
-                className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
-              >
-                Post Production
-              </p>
-              <ul
-                className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
-              >
-                <li>Editing Video Documentation</li>
-              </ul>
-            </div>
-          </div>
-          {/* Right Box */}
-
-          <div className="relative h-[400px] w-[370px]">
-            <Image
-              src={hut56}
-              alt="gambar YouTube hut56"
-              fill
-              className="object-cover"
-            />
-          </div>
-        </div>
-      </div>
-      {/* Small Screen */}
-      <div className="flex flex-col gap-3 justify-center px-2 py-8 w-full border-t-2 border-white/40 sm:hidden">
-        <p className="text-center text-3xl font-[700] leading-[1.0] text-tertiery">
-          FUN MORNING BULOG
-        </p>
-        <div className="w-full">
-          <div className="relative mx-auto h-[260px] w-[250px]">
-            <Image
-              src={hut56}
-              alt="gambar YouTube hut56"
-              fill
-              className="object-fit"
-            />
-          </div>
-
-          <p
+            <p
             className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
           >
             Pre&#45;Production
@@ -107,11 +40,12 @@ function Page10Brand() {
           <ul
             className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
           >
-            <li>Concepting &#40;Rundown &#38; Script Writing&#41;</li>
+            <li>Concepting </li>
+            <li>Rundown and Script Writing</li>
+            <li>Set Drama</li>
             <li>Stage Design</li>
+            <li>Games Concept</li>
             <li>Property</li>
-            <li>Location</li>
-            <li>Talent Search</li>
           </ul>
           <p
             className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
@@ -122,10 +56,10 @@ function Page10Brand() {
             className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
           >
             <li>Manpower Team</li>
-            <li>Show Directing</li>
+            <li>Show Management</li>
             <li>Talent Brief</li>
             <li>Technical System</li>
-            <li>Documentation photo and video</li>
+            <li>Documentation</li>
           </ul>
           <p
             className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
@@ -135,7 +69,74 @@ function Page10Brand() {
           <ul
             className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
           >
-            <li>Editing Video Documentation</li>
+            <li>Aftermovie and Photo Catalogue</li>
+          </ul>
+            </div>
+          </div>
+          {/* Right Box */}
+
+          <div className="relative h-[350px] w-[600px]">
+            <Image
+              src={milenial}
+              alt="gambar YouTube milenial"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </div>
+      {/* Small Screen */}
+      <div className="flex flex-col gap-3 justify-center px-2 py-8 w-full border-t-2 border-white/40 sm:hidden">
+        <p className="text-center text-3xl font-[700] leading-[1.0] text-tertiery">
+          MILLENIALS GATHERING BULOG
+        </p>
+        <div className="w-full">
+          <div className="relative mx-auto h-[210px] w-full">
+            <Image
+              src={milenial}
+              alt="gambar YouTube milenial"
+              fill
+              className="object-fit"
+            />
+          </div>
+          <p
+            className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            Pre&#45;Production
+          </p>
+          <ul
+            className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            <li>Concepting </li>
+            <li>Rundown and Script Writing</li>
+            <li>Set Drama</li>
+            <li>Stage Design</li>
+            <li>Games Concept</li>
+            <li>Property</li>
+          </ul>
+          <p
+            className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            Production
+          </p>
+          <ul
+            className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            <li>Manpower Team</li>
+            <li>Show Management</li>
+            <li>Talent Brief</li>
+            <li>Technical System</li>
+            <li>Documentation</li>
+          </ul>
+          <p
+            className={`mt-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            Post Production
+          </p>
+          <ul
+            className={`ml-5 mt-2 flex list-disc flex-col gap-1 px-2 text-lg font-[200] text-white ${outfit.className}`}
+          >
+            <li>Aftermovie and Photo Catalogue</li>
           </ul>
         </div>
       </div>
