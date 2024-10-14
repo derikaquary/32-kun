23521d92f76b4b79f928b66296849238197687a2
derikaquary <derikaquary@gmail.com>
2024-10-14 02:22:39 +0000

    added the name and position of the team members to the team page.

--- app/team/page.js
+++ app/team/page.js
@@ -14,7 +14,7 @@ function Team() {
   return (
     <div>
       {/* Big Screen */}
-      <div className="relative hidden h-[100vh] w-full flex-col items-center justify-center gap-[60px] border-b-2 border-white/30 pt-[70px] sm:flex">
+      <div className="relative hidden h-[100vh] w-full flex-col items-center justify-center gap-[30px] border-b-2 border-white/30 pt-[50px] sm:flex">
         {" "}
         <Image
           src={page1}
@@ -26,27 +26,59 @@ function Team() {
           OUR TEAM
         </p>
         <div className="flex flex-col">
-          <div className="relative h-[310px] w-[270px] mx-auto mb-[-90px]">
-            <Image src={person1} alt="person1 logo" fill className="object-fit" />
-          </div>
-          <div className="flex items-center gap-[500px] mt-[-160px]">
-            <div className="relative h-[310px] w-[270px]">
-              <Image src={person2} alt="person2 logo" fill className="object-fit" />
-            </div>
-            <div className="relative h-[310px] w-[270px]">
+          <div className="flex flex-col">
+            <div className="relative mx-auto h-[210px] w-[175px]">
               <Image
-                src={person3}
-                alt="person3 logo"
+                src={person1}
+                alt="person1 logo"
                 fill
                 className="object-fit"
               />
+            </div>
+            <p className="w-full text-center text-lg font-[600] text-primary">
+              M. RIZKI ARIA BIMA
+            </p>
+            <p className="text-sm font-bold text-center text-black">CEO</p>
+          </div>
+          <div className="mt-[-160px] flex items-center gap-[300px]">
+            <div className="flex flex-col">
+              <div className="relative mx-auto h-[210px] w-[175px]">
+                <Image
+                  src={person2}
+                  alt="person2 logo"
+                  fill
+                  className="object-fit"
+                />
+              </div>
+              <p className="w-full text-center text-lg font-[600] text-primary">
+                DEA DESTRIANA
+              </p>
+              <p className="text-sm font-bold text-center text-black">
+                CREATIVE DIRECTOR
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <div className="relative mx-auto h-[210px] w-[175px]">
+                <Image
+                  src={person3}
+                  alt="person3 logo"
+                  fill
+                  className="object-fit"
+                />
+              </div>
+              <p className="w-full text-center text-lg font-[600] text-primary">
+                FERDY AL&#39;AMIN
+              </p>
+              <p className="text-sm font-bold text-center text-black">
+                TECHNICAL DIRECTOR
+              </p>
             </div>
           </div>
         </div>
       </div>
 
       {/* Small Screen */}
-      <div className="relative flex w-full flex-col items-center justify-center border-b-2 border-white/30 pt-[80px] sm:hidden">
+      <div className="relative flex w-full flex-col items-center justify-center border-b-2 border-white/30 pt-[80px] pb-4 sm:hidden">
         {" "}
         <Image
           src={page1}
@@ -58,24 +90,32 @@ function Team() {
           <p className="text-center text-6xl font-[900] text-primary">
             OUR TEAM
           </p>
-          <div className="relative h-[290px] w-[270px]">
-            <Image src={person1} alt="kun logo" fill className="object-fit" />
+          <div className="flex flex-col">
+            <div className="relative h-[300px] w-[270px]">
+              <Image src={person1} alt="kun logo" fill className="object-fit" />
+            </div>
+            <p className="w-full text-center text-lg font-[600] text-primary">
+              M. RIZKI ARIA BIMA
+            </p>
+            <p className="font-bold text-center text-black">CEO</p>
           </div>
-          <div className="relative h-[290px] w-[270px]">
-            <Image
-              src={person2}
-              alt="person2 logo"
-              fill
-              className="object-fit"
-            />
+          <div className="flex flex-col">
+            <div className="relative h-[300px] w-[270px]">
+              <Image src={person2} alt="kun logo" fill className="object-fit" />
+            </div>
+            <p className="w-full text-center text-lg font-[600] text-primary">
+            DEA DESTRIANA
+            </p>
+            <p className="font-bold text-center text-black">CREATIVE DIRECTOR</p>
           </div>
-          <div className="relative h-[290px] w-[270px]">
-            <Image
-              src={person3}
-              alt="person3 logo"
-              fill
-              className="object-fit"
-            />
+          <div className="flex flex-col">
+            <div className="relative h-[300px] w-[270px]">
+              <Image src={person3} alt="kun logo" fill className="object-fit" />
+            </div>
+            <p className="w-full text-center text-lg font-[600] text-primary">
+            FERDY AL&#39;AMIN
+            </p>
+            <p className="font-bold text-center text-black">TECHNICAL DIRECTOR</p>
           </div>
         </div>
       </div>
